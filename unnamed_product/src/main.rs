7794d391b5b4f0b297133eb321e5a6cadc8ca8f4
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-03-12 21:08:15 +0100

    [TEST] added valid check function test

--- unnamed_product/src/main.rs
+++ unnamed_product/src/main.rs
@@ -1,5 +1,5 @@
 /*
-* @author Jakob Grätz
+* @author Jakob Grätz, Johannes Schießl
 * @edition 12/03/2024 DD/MM/YYYY
 * @version v0.0.1
 * @description Main Rust file for backend.
@@ -9,7 +9,5 @@ mod db_handler;
 mod api_utils;
 
 fn main() {
-    api_utils::handle_api_utils_mod_test();
-    db_handler::handle_db_mod_test();
-    println!("Hello, world!");
+    api_utils::check_api_request("abc123".to_string(), "John Doe".to_string(), "2024-04-12");
 }
