dbd3d792c0beb3058b137da8a775a2e0941f88aa
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-13 17:37:57 +0100

    [DEV] implemented api key gen, todo: api key in db

--- unnamed_product/src/api_utils.rs
+++ unnamed_product/src/api_utils.rs
@@ -1,4 +1,16 @@
 /*
+* @author Jakob Grätz, Johannes Schießl
+* @edition 13/03/2024 DD/MM/YYYY
+* @version v0.0.1
+* @description Rust file for api utilities.
+*/
+
+use rand::Rng;
+use std::collections::HashSet;
+
+const KEY_LENGTH: usize = 32;
+
+/*
 * @author Jakob Grätz
 * @description Checks if a given API key is a valid API key.
 */
@@ -15,6 +27,20 @@ pub fn check_api_key(api_key: String) -> bool {
 * @description Generates a new API key (String).
 */
 pub fn generate_api_key() -> String {
-    let api_key = "My API key".to_string();
-    return api_key;
+    let mut rng = rand::thread_rng();
+
+    let charset: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!+-#";
+    let mut key = String::with_capacity(KEY_LENGTH);
+
+    let mut unique_chars = HashSet::with_capacity(KEY_LENGTH);
+    while key.len() < KEY_LENGTH {
+        // random index within charset range
+        let random_index = rng.gen_range(0..charset.len());
+        // random character from charset
+        let random_char = charset[random_index] as char;
+        if unique_chars.insert(random_char) {
+            key.push(random_char);
+        }
+    }
+    return key;
 }
