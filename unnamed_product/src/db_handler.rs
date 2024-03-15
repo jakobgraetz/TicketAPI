8080b0ea0a9b8e3cd665dc214399abd675a6100a
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-15 14:24:51 +0100

    initialized db_handler.rs

---
+++ unnamed_product/src/db_handler.rs
@@ -0,0 +1,6 @@
+/*
+* @author Jakob Grätz, Johannes Schießl
+* @edition 15/03/2024 DD/MM/YYYY
+* @version v0.0.1
+* @description Rust file responsible for handling MongoDB (atlas) connection and databases.
+*/
