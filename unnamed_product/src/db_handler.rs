11eab3d10c48488b54a9d03af751af5037b0c933
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-16 10:48:50 +0100

    [DOC] added note

--- unnamed_product/src/db_handler.rs
+++ unnamed_product/src/db_handler.rs
@@ -3,4 +3,5 @@
 * @edition 15/03/2024 DD/MM/YYYY
 * @version v0.0.1
 * @description Rust file responsible for handling MongoDB (atlas) connection and databases.
+* @note The allowed IP in the Atlas Web / DB deployment may needs to be adjusted based on the server IP ...
 */
