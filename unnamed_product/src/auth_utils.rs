5ed31527894b459b146c35f38d4d05f6b76d6210
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-10 17:16:40 +0200

    [DEV]

---
+++ unnamed_product/src/auth_utils.rs
@@ -0,0 +1,12 @@
+/*
+* @author Jakob Grätz, Johannes Schießl | @jakobgraetz, @johannesschiessl
+* @edition 10/04/2024 DD/MM/YYYY
+* @version v0.0.1
+* @description Rust file for authentication utilities.
+*/
+
+mod api_utils;
+
+// Hash password
+
+// Hash API Key
