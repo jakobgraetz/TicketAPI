5a9bf9796a430e909fc0af60eeb653be49b9d877
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-11 16:43:25 +0200

    [DEV] a

--- unnamed_product/src/db_handler.rs
+++ unnamed_product/src/db_handler.rs
@@ -1,6 +1,6 @@
 /*
 * @author Jakob Grätz, Johannes Schießl | @jakobgraetz, @johannesschiessl
-* @edition 10/04/2024 DD/MM/YYYY
+* @edition 11/04/2024 DD/MM/YYYY
 * @version v0.0.1
 * @description Rust file responsible for handling MongoDB (atlas) connection and databases.
 * @note The allowed IP in the Atlas Web / DB deployment may needs to be adjusted based on the server or rather, client, IP ... also export MONGODB_URI env var
