da965ee34a742de74335797e5c78465d77f5b24d
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-13 10:01:37 +0200

    [DEV] started implementing password hashing

--- unnamed_product/src/auth_utils.rs
+++ unnamed_product/src/auth_utils.rs
@@ -5,8 +5,29 @@
 * @description Rust file for authentication utilities.
 */
 
-mod api_utils;
+extern crate argon2;
+use argon2::{self, Config, ThreadMode, Variant, Version};
 
 // Hash password
+pub fn hash_password (password: String) -> Result<(String, String), argon2::Error> {
+    // Generates a cryptographically secure salt string
+    let salt = argon2::generate_salt();
 
+    // config for hashing the password
+    let config = Config {
+        variant: Variant::Argon2id,
+        version: Version::Version13,
+        mem_cost: 65536,
+        time_cost: 10,
+        lanes: 4,
+        thread_mode: ThreadMode::Parallel,
+        secret: &[],
+        ad: &[],
+        hash_length: 32,
+    };
+
+    let hash = argon2::hash_encoded(password.as_bytes(), &salt, &config)?;
+
+    Ok(hash, hex::encode(&salt))
+}
 // Hash API Key
