dc93f20b8ffe19f5d9648b9ff07408a5f2493c89
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-13 14:29:28 +0200

    [DEV] desperately trying to fix my fn

--- unnamed_product/src/auth_utils.rs
+++ unnamed_product/src/auth_utils.rs
@@ -9,7 +9,8 @@ extern crate argon2;
 use argon2::{
     password_hash::{
         rand_core::OsRng,
-        PasswordHash, PasswordHasher, PasswordVerifier, SaltString, Salt
+        /*PasswordHash,*/ PasswordHasher, /*PasswordVerifier,*/ SaltString, /*Salt*/
+        Error
     },
     Argon2
 };
@@ -21,25 +22,11 @@ pub fn hash_string (data: String) -> Result<(String, SaltString), argon2::passwo
     let argon2 = Argon2::default();
 
     let data_hash = argon2.hash_password(data.as_bytes(), &salt)?;
-    let hash = data_hash.hash;
+    let hash = match data_hash.hash {
+        Some(h) => h.to_string(),
+        None => "exception".to_string(), //TODO: error handling wizardry
+    };
     println!("{:?}", hash);
     println!("{:?}", salt);
-    Ok((hash.expect("error with hashing").to_string(), salt))
-}
-
-pub fn check_hash_string () -> Result<String, argon2::password_hash::Error> {
-    let my_hash: String = "8ysWb2FRB+pnE77Iro1oUoaBAqJS6I+/yr1FesA2kqY".to_string();
-    let my_salt = SaltString::new("qGgbFKPpXQMYOq7qF5CFDA");
-
-    let password: String = "A quick brown fox jumps over the lazy frog. 123456789!?".to_string();
-
-    let argon2 = Argon2::default();
-
-    let salt = Salt::from(&my_salt);
-    let my_hash = argon2.hash_password(password.as_bytes(), salt)?;
-    let hash = my_hash.hash;
-
-    println!("{:?}", hash);
-
-    Ok(hash.expect("error").to_string())
+    Ok((hash, salt))
 }
