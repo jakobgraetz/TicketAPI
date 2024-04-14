bf8dd1f21673f6a8b6390589cd3d63b91d6bc003
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-14 12:02:47 +0200

    [DEV]

--- ignotum_tickets/src/main.rs
+++ ignotum_tickets/src/main.rs
@@ -12,6 +12,7 @@ mod auth_utils;
 // THIS CANNOT; AND I REPEAT: CANNOT; BE DELETED IN ANY WAY SHAPE OR FORM!!!!!!
 #[tokio::main]
 async fn main() {
+    db_handler::delete_user("jane.doe@example.com".to_string());
     println!("{:?}", db_handler::get_user_data("john.doe@example.com".to_string()).await);
     let hash_tuple = auth_utils::hash_string("A quick brown fox jumps over the lazy frog. 123456789!?".to_string()).unwrap();
     println!("[DEV] testing str hasher with str 'A quick brown fox jumps over the lazy frog. 123456789!?' -> {:?} with salt: {:?}",
