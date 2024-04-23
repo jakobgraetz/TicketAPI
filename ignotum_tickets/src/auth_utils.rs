631c66be6e60b29d632722ef2865c339b8a00fa0
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-04-23 18:07:52 +0200

    [DEV] updated disclaimer

--- ignotum_tickets/src/auth_utils.rs
+++ ignotum_tickets/src/auth_utils.rs
@@ -1,6 +1,6 @@
 /*
 * @author Jakob Grätz, Johannes Schießl | @jakobgraetz, @johannesschiessl
-* @edition 14/04/2024 DD/MM/YYYY
+* @edition 23/04/2024 DD/MM/YYYY
 * @version v0.0.1
 * @description Rust file for authentication utilities, like hashing, key gen, ...
 */
