7f86a269f42277fb134ef1bf06965ad6a4d1e3fd
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-05-03 06:42:11 +0200

    [DEV] updated disclaimer

--- ignotum_tickets/src/main.rs
+++ ignotum_tickets/src/main.rs
@@ -1,6 +1,6 @@
 /*
 * @author Jakob Grätz, Johannes Schießl | @jakobgraetz, @johannesschiessl
-* @edition 27/04/2024 DD/MM/YYYY
+* @edition 03/05/2024 DD/MM/YYYY
 * @version v0.0.1
 * @description Main Rust file for backend.
 */
