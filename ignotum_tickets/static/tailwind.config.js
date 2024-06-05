a173a81d4625f4a05f49fddfb7c46d71123823fe
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-06-05 17:31:33 +0200

    update

--- ignotum_tickets/static/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["../ignotum_tickets/templates/*.{tera,html}", "../ignotum_tickets/src/*.rs", "./*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
