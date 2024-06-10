89ea590197c3256a4ce8b09043728d92435e79d2
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-06-10 17:07:41 +0200

    update

--- ignotum_tickets/src/db_handler.rs
+++ ignotum_tickets/src/db_handler.rs
@@ -4,9 +4,7 @@
 * @version v0.0.1
 * @description Rust file responsible for handling MongoDB (atlas) connection and databases.
 * @note The allowed IP in the Atlas Web / DB deployment may needs to be adjusted based on the server or rather, client, IP ... also export MONGODB_URI env var
-*/
 
-/*
 Some good docs for Rust MongoDB:
 https://mongodb.github.io/mongo-rust-driver/manual/reading.html
 https://taharmeijs.medium.com/beginners-guide-to-mongodb-and-rust-8d8d3ef17920
